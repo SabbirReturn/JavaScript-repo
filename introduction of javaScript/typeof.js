9bf856d2ad67abda2c5b9e984ad9429aa140e8af
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-08-25 15:17:50 -0500

    check the type of variable

---
+++ introduction of javaScript/typeof.js
@@ -0,0 +1,8 @@
+let number = 28;
+let name = ' Md Sabbir Hasan';
+let isPoor = true;
+console.log(number,name,isPoor);
+
+console.log(typeof number);
+console.log(typeof name);
+console.log(typeof isPoor);;
