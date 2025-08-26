4e071eaa6210abed45828d96a9f946811b3a4a10
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-08-26 16:13:40 -0500

    problem1

--- introduction of javaScript/operation.js
+++ introduction of javaScript/operation.js
@@ -1,5 +1,15 @@
-let price = '45';
-price = parseInt(price)+ 10;
-let newPrice = price + 50;
-let discountPrice = newPrice - 10;
-console.log(discountPrice);
+// let price = '45';
+// price = parseInt(price)+ 10;
+// let newPrice = price + 50;
+// let discountPrice = newPrice - 10;
+// console.log(discountPrice);
+
+// short hand
+
+let price = 50;
+price +=10;
+// console.log(price);
+
+let x = '10.5';
+let num = Number(x);
+console.log(num);
---
+++ introduction of javaScript/problem1.js
@@ -0,0 +1,6 @@
+let myMoney = 1000;
+let orangePrice = 400;
+let applePrice = 300;
+let totalPrice = orangePrice + applePrice;
+let returnMoney = myMoney - totalPrice;
+console.log(returnMoney);
