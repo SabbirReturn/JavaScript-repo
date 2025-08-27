84db397383063b5f5a797a1ed9e8ae6aa8af795a
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-08-27 16:35:36 -0500

    get average

---
+++ introduction of javaScript/problem2.js
@@ -0,0 +1,10 @@
+let mathematics = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.45;
+let bangla = 99.50;
+
+let totalNumber = mathematics + biology + chemistry + physics + bangla;
+
+let averageNumber = totalNumber / 5;
+console.log(averageNumber.toFixed(2));
