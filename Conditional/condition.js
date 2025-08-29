51828ab11f160e81ebae2986ded380eadba40f6d
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-08-29 16:53:51 -0500

    condition

---
+++ Conditional/condition.js
@@ -0,0 +1,16 @@
+let birianyPrice = 300;
+if(birianyPrice<400){
+    // console.log('Give me a packet')
+}
+else{
+//    console.log('No need to a packet') 
+}
+
+
+let marks = 86;
+if(marks<78){
+    console.log('Got A+')
+}
+else{
+    console.log('Fail')
+}
--- Conditional/index.js
+++ Conditional/index.js
@@ -2,5 +2,7 @@ let price = 500;
 let newPrice = 700;
 
 if(price<600){
-    console.log(newPrice);
+    // console.log(newPrice);
 }
+
+console.log(10==='10')
