fda2b3ac010ac73086cade6bf1511c091db7b06d
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-08-31 12:38:26 -0500

    sabbir

--- Conditional/condition.js
+++ Conditional/condition.js
@@ -1,16 +1,25 @@
-let birianyPrice = 300;
-if(birianyPrice<400){
-    // console.log('Give me a packet')
-}
-else{
-//    console.log('No need to a packet') 
-}
+// let birianyPrice = 300;
+// if(birianyPrice<400){
+//     // console.log('Give me a packet')
+// }
+// else{
+// //    console.log('No need to a packet') 
+// }
 
 
 let marks = 86;
 if(marks<78){
-    console.log('Got A+')
+    // console.log('Got A+')
 }
 else{
-    console.log('Fail')
+    // console.log('Fail')
+}
+
+
+let birianyPrice = 300;
+if(birianyPrice<400){
+    console.log('Give me a packet')
+}
+else{
+   console.log('No need to a packet') 
 }
