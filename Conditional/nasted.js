2d95820958b580ccc2bbe58e83fa3b7d8c26d4db
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-14 09:01:32 -0500

    nasted

---
+++ Conditional/nasted.js
@@ -0,0 +1,42 @@
+// let money = 000000;
+// if(money>300){
+//     console.log('You are borolox')
+// }
+// else{
+//     if(money>100){
+//         console.log('Tumi borolox o na gorib o na')
+//     }
+//     else{
+//         if(money>0){
+//             console.log('Kola khaw')
+//         }
+//         else{
+//             console.log('Tui amar bondhu na')
+//         }
+//     }
+// }
+
+
+let marks = 40;
+
+if(marks>=80){
+    console.log('You Got A+')
+}
+else{
+    if(marks<=79 && marks>=70){
+        console.log('You got A')
+    }
+    else{
+        if(marks<=69 && marks>=60){
+            console.log('You got A-')
+        }
+        else{
+            if(marks<=59 && marks>=50){
+                console.log('You got B')
+            }
+            else{
+                console.log('You got F')
+            }
+        }
+    }
+}
