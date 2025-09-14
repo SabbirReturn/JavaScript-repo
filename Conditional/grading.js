a2e7ac9212038a1b8d5e4803d97f2f99e3a82901
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-14 07:53:58 -0500

    grading

---
+++ Conditional/grading.js
@@ -0,0 +1,22 @@
+let marks = 97;
+if(marks>=80){
+    console.log('You Got A+')
+}
+else if(marks<=79 && marks>=70){
+    console.log('You Got A')
+}
+else if(marks<=69 && marks>=60){
+    console.log('You Got A-')
+}
+else if(marks<=59 && marks>=50){
+    console.log('You Got B')
+}
+else if(marks<=49 && marks>=40){
+    console.log('You Got B-')
+}
+else if(marks<=39 && marks>=33){
+    console.log('You Got C')
+}
+else{
+    console.log('You Got F')
+}
