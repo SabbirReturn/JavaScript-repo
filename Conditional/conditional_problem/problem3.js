3132ccc37a01bf296798dd869232395d4b01e7f9
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-16 17:38:55 -0500

    greading system

---
+++ Conditional/conditional_problem/problem3.js
@@ -0,0 +1,16 @@
+let marks = 58;
+if(marks>=90){
+    console.log('You got A')
+}
+else if(marks<=89 && marks>=80){
+    console.log('Yot got B')
+}
+else if(marks<=79 && marks>=70){
+    console.log('Yot got B')
+}
+else if(marks<=69 && marks>=60){
+    console.log('Yot got B')
+}
+else{
+    console.log('You got F')
+}
