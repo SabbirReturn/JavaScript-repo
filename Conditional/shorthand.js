8a66dde3f519785453a124f5a685b266f4d245d9
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-16 12:29:39 -0500

    again

--- Conditional/shorthand.js
+++ Conditional/shorthand.js
@@ -62,4 +62,38 @@ let isRich = false;
 //     console.log('I will try')
 // }
 
+// !isRich ? console.log('congratulation') : console.log('I will try')
+
+
+// let isFail = true;
+// if(isFail){
+//     // console.log('Kopal Kharap')
+// }
+// else{
+//     // console.log('Beche geci')
+// }
+
+// let isRich = false;
+// if(!isRich){
+//     console.log('Congratulation')
+// }
+// else{
+//     console.log('I will try')
+// }
+// let isFail = true;
+// if(isFail){
+//     // console.log('Kopal Kharap')
+// }
+// else{
+//     // console.log('Beche geci')
+// }
+
+let isRich = false;
+// if(!isRich){
+//     console.log('Congratulation')
+// }
+// else{
+//     console.log('I will try')
+// }
+
 !isRich ? console.log('congratulation') : console.log('I will try')
