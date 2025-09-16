37d568c344e8b40127b5947e14b798101f11fa87
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-16 12:29:15 -0500

    again

--- Conditional/shorthand.js
+++ Conditional/shorthand.js
@@ -1,3 +1,36 @@
+// // let isFail = true;
+// // if(isFail){
+// //     // console.log('Kopal Kharap')
+// // }
+// // else{
+// //     // console.log('Beche geci')
+// // }
+
+// // let isRich = false;
+// // if(!isRich){
+// //     console.log('Congratulation')
+// // }
+// // else{
+// //     console.log('I will try')
+// // }
+// // let isFail = true;
+// // if(isFail){
+// //     // console.log('Kopal Kharap')
+// // }
+// // else{
+// //     // console.log('Beche geci')
+// // }
+
+// let isRich = false;
+// // if(!isRich){
+// //     console.log('Congratulation')
+// // }
+// // else{
+// //     console.log('I will try')
+// // }
+
+// !isRich ? console.log('congratulation') : console.log('I will try')
+
 // let isFail = true;
 // if(isFail){
 //     // console.log('Kopal Kharap')
