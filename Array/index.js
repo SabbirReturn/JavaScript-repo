8a81e992f74ddb32af7b0a4b0c31eaa9adfe794b
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-17 14:37:41 -0500

    using length and index

---
+++ Array/index.js
@@ -0,0 +1,6 @@
+let nums = [22,3,4,545356,324,65436,21223453245]
+// let num1 = nums[1];
+nums[1] = 100000
+let num1 = nums[1]
+console.log(nums)
+console.log(num1)
---
+++ Array/length.js
@@ -0,0 +1,3 @@
+let numbers = [1,2,3,4,5,56,6,7]
+let length = numbers.length;
+console.log(length)
