29d00eeb71f20d6689df34e67bf2869c1c3938cf
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-17 04:34:56 -0500

    problem5.js

---
+++ Conditional/conditional_problem/problem5.js
@@ -0,0 +1,13 @@
+let num1 = 62;
+let num2 = 52;
+let result;
+// if(num1>num2){
+//     result = num1 *2;
+// }
+// else{
+//     result = num1 + num2;
+// }
+// console.log('Value:' +result)
+
+result= num1> num2 ? num1 * 2 : num1 + num2
+console.log('Value:' +result)
