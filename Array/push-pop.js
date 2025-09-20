c7253a0122c5d68bdcb7b2f82fc5f996b426159e
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-20 16:38:47 -0500

    push-pop

--- Array/push-pop.js
+++ Array/push-pop.js
@@ -1,4 +1,16 @@
-let friends = ['Sabbir','Hasan','Robin','Mahfuza'];
+// let friends = ['Sabbir','Hasan','Robin','Mahfuza'];
+// // console.log(friends)
+// // friends.push = 'Misu'
+// // console.log(friends)
+
+// let friend = friends.push
+// friend = 'Nayem'
+// // friends.pop()
 // console.log(friends)
-friends.push = 'Misu'
+
+let friends = ['sabbir','hasan','robin']
+console.log(friends)
+friends.push('misu')
+console.log(friends)
+friends.pop()
 console.log(friends)
