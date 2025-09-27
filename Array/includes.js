2c26b227a66f20fe52ac92b763dfbb8d3d576472
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-27 16:27:11 -0500

    includes

---
+++ Array/includes.js
@@ -0,0 +1,9 @@
+let friends = ['Sabbir','Hasan','Hasib','Ovi'];
+// console.log(friends.includes('Ovi'))
+
+if(friends.includes('ovi')){
+    console.log('Go to pakshi Bridge')
+}
+else{
+    console.log('Na jabo na')
+}
