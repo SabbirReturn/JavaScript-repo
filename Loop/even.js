f68366e51b5d5a1c9eafaa294cc21902c11780d9
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-29 12:47:19 -0500

    even

--- Loop/even.js
+++ Loop/even.js
@@ -1,3 +1,15 @@
+// let num = 1;
+// let evenSum = 0
+// while(num <= 10){
+//     // console.log(num)
+//     if(num % 2 === 0){
+//         console.log('Even number:', num)
+//         evenSum = evenSum + num
+//         console.log('evensum:', evenSum)
+//     }
+    
+//     num++
+// }
 let num = 1;
 let evenSum = 0
 while(num <= 10){
