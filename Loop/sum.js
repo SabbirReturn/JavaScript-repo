42c29a3af0e6361abaa066d28011799c0b4544fd
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-29 12:46:54 -0500

    sum

--- Loop/sum.js
+++ Loop/sum.js
@@ -1,3 +1,11 @@
+// let num = 0;
+// let sum = 0
+// while(num<=20){
+//     console.log(num)
+//     sum = sum + num
+//     console.log("sum:", sum)
+//     num++
+// }
 let num = 0;
 let sum = 0
 while(num<=20){
