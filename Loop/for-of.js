ff74514178fa49a9f359d5be8a9a1d2064d86f9f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-09-29 12:47:46 -0500

    for-f

--- Loop/for-of.js
+++ Loop/for-of.js
@@ -1,3 +1,9 @@
+// let numbers = [12,13,14,15,16,17,18,19];
+// console.log(numbers)
+// for(let num of numbers){
+//     console.log(num)
+// }
+
 let numbers = [12,13,14,15,16,17,18,19];
 console.log(numbers)
 for(let num of numbers){
