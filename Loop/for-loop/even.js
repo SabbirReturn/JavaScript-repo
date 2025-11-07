f9c436907714f5ce00c1c1fa65614d3358eba549
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-07 18:07:14 +0600

    even

---
+++ Loop/for-loop/even.js
@@ -0,0 +1,9 @@
+for( let num = 0; num<50; num++){
+    // console.log(num)
+    if( num % 2 ===0){
+        // console.log('Even Num:', num)
+        if(num % 5 === 0){
+            console.log('Most Important Number:',num)
+        }
+    }
+}
---
+++ Loop/for-loop/for.js
@@ -0,0 +1,6 @@
+// structure:
+
+
+for(let num = 0; num<10; num++){
+    // console.log(num)
+}
