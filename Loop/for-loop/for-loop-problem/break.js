9dbdccb0caf24b251a5d10f4a8a9e29bb472b781
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-09 00:50:12 +0600

    break and conitue

---
+++ Loop/for-loop/for-loop-problem/break.js
@@ -0,0 +1,15 @@
+for(let i = 1; i<200; i++){
+    if( i > 5){
+        break;
+    }
+    // console.log(i)
+}
+// console.log('Life is Break')
+
+
+for(let i = 55; i>40; i--){
+    if(i<50){
+        break
+    }
+    console.log(i)
+}
