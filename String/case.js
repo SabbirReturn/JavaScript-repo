922bd321f6d6d8af5a1ad23ba481672b2010505f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-10 03:53:36 +0600

    case

---
+++ String/case.js
@@ -0,0 +1,19 @@
+let exam = 'Chemistry';
+let book = 'chemistry';
+if(exam.toLowerCase() === book.toLowerCase()){
+    // console.log('Pass')
+}
+else{
+    // console.log('Fail')
+}
+
+
+let drink ='Water';
+let liquid = '  water'
+
+if(drink.trim().toLowerCase()===liquid.trim().toLowerCase()){
+    console.log('Santi paici')
+}
+else{
+    console.log('Test less')
+}
