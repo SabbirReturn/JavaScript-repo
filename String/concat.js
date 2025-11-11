8468dec198e7fbabdbf41f499dd61cc42da3a84c
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-11 20:06:28 +0600

    slice,split,join,concat

---
+++ String/concat.js
@@ -0,0 +1,12 @@
+let firstName = 'Sabbir';
+let secondName = 'Hasan';
+let fullName = firstName.concat(" ",secondName)
+// console.log(fullName)
+
+let name = ['Sabbir','Hasan','Ratul','Rupok','Mahim'];
+if(name.includes('Ratul')){
+    console.log('Lets go to mawya')
+}
+else{
+    console.log('Jabo na')
+}
---
+++ String/slice_split.js
@@ -0,0 +1,10 @@
+let address = 'Uttara';
+let word = address.slice(0,3);
+// console.log(word)
+
+
+let name = 'Md Sabbir Hasan';
+let alphabet = name.split(' ')
+let fullName = alphabet.join('+')
+console.log(alphabet)
+console.log(fullName)
