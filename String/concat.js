ef38a79375f27639649f47d5816780a2448e8f98
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-11 20:43:12 +0600

    concat, includes

--- String/concat.js
+++ String/concat.js
@@ -3,10 +3,34 @@ let secondName = 'Hasan';
 let fullName = firstName.concat(" ",secondName)
 // console.log(fullName)
 
-let name = ['Sabbir','Hasan','Ratul','Rupok','Mahim'];
-if(name.includes('Ratul')){
-    console.log('Lets go to mawya')
+// let name = ['Sabbir','Hasan','Ratul','Rupok','Mahim'];
+// if(name.includes('Ratul')){
+//     // console.log('Lets go to mawya')
+// }
+// else{
+//     // console.log('Jabo na')
+// }
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+arr2.push(7)
+// console.log(arr2)
+let fullArray = arr1.concat(arr2);
+// console.log(fullArray)
+
+let animals = ["cat", "dog", "elephant", "lion"];
+
+if (animals.includes("tiger")) {
+//   console.log("Tiger is in the list!");
+} else {
+//   console.log("Tiger is not in the list.");
 }
-else{
-    console.log('Jabo na')
+
+
+let name = "JavaScript";
+
+if (name.includes("Script")) {
+  console.log("The word 'Script' exists in the string.");
+} else {
+  console.log("The word 'Script' does not exist in the string.");
 }
--- String/slice_split.js
+++ String/slice_split.js
@@ -1,10 +1,47 @@
 let address = 'Uttara';
-let word = address.slice(0,3);
+// let word = address.slice(0,3);
 // console.log(word)
 
 
 let name = 'Md Sabbir Hasan';
 let alphabet = name.split(' ')
 let fullName = alphabet.join('+')
-console.log(alphabet)
-console.log(fullName)
+// console.log(alphabet)
+// console.log(fullName)
+
+
+// practice
+
+
+let fruits = ["apple", "banana", "cherry", "mango", "orange"];
+let middleThreeFruits = fruits.slice(1,4)
+// console.log(middleThreeFruits)
+
+let text = "JavaScriptProgramming";
+// let word = text.slice(4,10)
+// console.log(word)
+
+let sentence = "I love learning JavaScript every day";
+let singleWord = sentence.split(' ')
+let thirdWord = singleWord.slice(2,3)
+// console.log(thirdWord)
+// console.log(singleWord)
+
+let date = "2025-11-09";
+let singleDate = date.split('-')
+let year = singleDate[0];
+let month = singleDate[1];
+let day = singleDate[2];
+// console.log('Year:',year)
+// console.log('Month:',month);
+// console.log('Day:', day)
+// console.log(singleDate)
+
+
+let words = ["Frontend", "Developer", "Challenge"];
+let fullSentence = words.join(' ')
+// console.log(fullSentence)
+
+let letters = ["H", "e", "l", "l", "o"];
+let word = letters.join('')
+console.log(word)
