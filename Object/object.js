f55d66ad6b3764c5c9c5221b32cd5ba1ab925d31
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-12 21:23:20 +0600

    object

---
+++ Object/object.js
@@ -0,0 +1,11 @@
+let man = {
+    name:'Md Sabbir Hasan',
+    age : 27,
+    address: 'Uttara sector 10, road number 18',
+    isStudent: true,
+}
+let propName = 'Profession';
+man[propName] = 'Developer'
+man[propName] = 'Devops'
+man['age'] = 30;
+console.log(man)
