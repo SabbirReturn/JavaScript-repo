92aceb6b2c3d9d9e624f922649871048cb93f3af
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-16 23:09:11 +0600

    function

---
+++ function/function.js
@@ -0,0 +1,28 @@
+// function square(num1){
+//     let double = num1 * num1
+//     return double;
+// }
+
+// // console.log(square(2))
+
+// function addNumber(num1, num2){
+//     let sum = num1 + num2
+//     return sum
+// }
+
+// let sumOfNumber = addNumber(55,80);
+// console.log(sumOfNumber)
+function square(num1){
+    let double = num1 * num1
+    return double;
+}
+
+// console.log(square(2))
+
+function addNumber(num1, num2){
+    let sum = num1 + num2
+    return sum
+}
+
+let sumOfNumber = addNumber(55,80);
+console.log(sumOfNumber)
