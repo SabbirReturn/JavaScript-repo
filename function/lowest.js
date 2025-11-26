77c131973a053c3b81de5ec074d85139c500b59e
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-11-26 20:15:02 +0600

    lowest

---
+++ function/lowest.js
@@ -0,0 +1,15 @@
+let lowestMarks = [75,66,68,95,78,69,52,78,108];
+
+
+function getLowest(numbers){
+    let lowestMark = numbers[0];
+    for(let number of numbers){
+        if(number<lowestMark){
+            lowestMark = number
+        }
+    }
+    return lowestMark;
+}
+
+let lowest = getLowest(lowestMarks);
+console.log(lowest)
