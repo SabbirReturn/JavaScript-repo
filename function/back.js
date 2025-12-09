ea73a161c48d071f6b81f8d53fd48736735e1256
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-12-10 02:32:59 +0600

    again

---
+++ function/back.js
@@ -0,0 +1,42 @@
+// function refine(nums){
+//     let evenNumber = [];
+//     let oddNumber = [];
+//     for(let num of nums){
+//         if(num % 2===0){
+//             evenNumber.push(num)
+//         }
+//         else{
+//             oddNumber.push(num)
+//         }
+//     }
+//     // return{
+//     //     even: evenNumber,
+//     //     odd: oddNumber
+//     // }
+//     return['even:',evenNumber, 'oddNumber',oddNumber]
+//     // return[`Even: ${evenNumber}, Odd: ${oddNumber}`]
+// }
+
+// let numbers = refine([1,2,3,4,5,6,7,8,9,10])
+// console.log(numbers);
+function refine(nums){
+    let evenNumber = [];
+    let oddNumber = [];
+    for(let num of nums){
+        if(num % 2===0){
+            evenNumber.push(num)
+        }
+        else{
+            oddNumber.push(num)
+        }
+    }
+    // return{
+    //     even: evenNumber,
+    //     odd: oddNumber
+    // }
+    return['even:',evenNumber, 'oddNumber',oddNumber]
+    // return[`Even: ${evenNumber}, Odd: ${oddNumber}`]
+}
+
+let numbers = refine([1,2,3,4,5,6,7,8,9,10])
+console.log(numbers);
