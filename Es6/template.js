e0361ee28545e8fb1b0ef31b635dc9d52af2c9f5
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-04 23:20:09 +0600

    again

--- Es6/template.js
+++ Es6/template.js
@@ -8,4 +8,5 @@ let fullName = `My name is ${first} ${second}`;
 function createBtn(text = 'click me', color = 'blue'){
     return ` <button style ="background: ${color}" > ${text}</button> `
 }
+// console.log(createBtn())
 console.log(createBtn())
