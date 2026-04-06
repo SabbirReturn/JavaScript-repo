b7ee29bb50d712bbc15fd2ab9a709f9b8cc07732
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-06 20:31:53 +0600

    arrow function

---
+++ Es6/arrow.js
@@ -0,0 +1,20 @@
+// function declaration
+
+function sum(a,b){
+    return a+b
+}
+
+// console.log(sum(10,20))
+
+// function expression
+
+let add1 = function(a,b){
+    return a +b;
+}
+// console.log(add1(12,11))
+
+
+// arrow function
+
+let add3 = (a,b) => a+b;
+console.log(add3(12,13))
--- Es6/template.js
+++ Es6/template.js
@@ -2,12 +2,10 @@ let first = 'Sabbir';
 let second = 'Hasan';
 
 let fullName = `My name is ${first} ${second}`;
-// console.log(fullName)
 
 
 function createBtn(text = 'click me', color = 'blue'){
     return ` <button style ="background: ${color}" > ${text}</button> `
 }
 
-// console.log(createBtn())
 console.log(createBtn())
