547c91cad247ee467da4845c0f583cd6ab99c3e2
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-08 20:51:09 +0600

    again

---
+++ Es6/keys-values.js
@@ -0,0 +1,29 @@
+let person ={
+    name:'Sabbir',
+    age: 28,
+    address:'Uttara sector 10'
+}
+
+function keyValue(person){
+    let keys = Object.keys(person)
+    let values = Object.values(person);
+    let result = []
+    for(let i = 0; i<keys.length; i++){
+        result.push(`${keys[i]} : ${values[i]}`)
+    }
+    return result
+}
+
+let ans = keyValue(person);
+// console.log(ans)
+
+let entries = Object.entries(person);
+// console.log(entries)
+
+function valueKey(object){
+    for(let [keys,values] of Object.entries(object)){
+        return(`${keys} : ${values}`)
+    }
+}
+let result = valueKey(person);
+console.log(result)
