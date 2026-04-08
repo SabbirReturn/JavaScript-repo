a4f3ae64c07d80a17bd2c9d96d8930930dc975fb
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-09 03:22:39 +0600

    problem

---
+++ problem/problem1.js
@@ -0,0 +1,18 @@
+let result =(a,b,c)=>{
+    let ans = a * b* c;
+    return ans
+};
+console.log(result(10,2,3))
+
+
+console.log(`I am web Developer.
+    I love code.
+    I love to eat biryani
+ `)
+
+
+let sum = (a,b=2)=>{
+    let result = a + b;
+    return result;
+}
+console.log(sum(10))    
---
+++ problem/problem2.js
@@ -0,0 +1,15 @@
+let checkEvenName = (friends)=>{
+    let evenArray = []
+    let oddArray = []
+    for(let friend of friends){
+        if(friend.length % 2 ===0){
+            evenArray.push(friend)
+        }
+        else{
+            oddArray.push(friend)
+        }
+    }
+   return [evenArray, oddArray]
+}
+let friendName = ['sabbir','hasan','robin','misu','haque','oysi']
+console.log(checkEvenName(friendName))
