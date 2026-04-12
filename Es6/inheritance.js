bd0fb705ec251e347ba3afdff4d80cc4dfcb9418
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-12 17:51:41 +0600

    practice

--- Es6/inheritance.js
+++ Es6/inheritance.js
@@ -25,6 +25,33 @@ class car extends bus{
 
 let car1 = new car('Toyota',1200000,'2002')
 let bus1 = new bus('Sb super Deluxe',6000000, 2022);
-bus1.using('desel')
-console.log(bus1)
-console.log(car1)
+// bus1.using('desel')
+// console.log(bus1)
+// console.log(car1)
+class animal{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    talking(talk){
+        console.log(`${this.name} is ${talk}`)
+    }
+}
+
+class dog extends animal{
+    constructor(name,age,price){
+        super(name,age);
+        this.price = price
+    }
+}
+
+class bird extends animal{
+    constructor(name,age,price){
+        super(name,age);
+        this.price = price
+    }
+}
+
+let bird1 = new bird('Kokil','1 year', 23000);
+bird1.talking('kuhu kuhu')
+console.log(bird1)
