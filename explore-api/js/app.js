9b57a65c8075500543184b352c88d5ed206d8d22
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-25 15:20:30 +0600

    callback funtion

---
+++ explore-api/js/app.js
@@ -0,0 +1,8 @@
+function user(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data=>displayUser(data))
+}
+function displayUser(data){
+    console.log(data)
+}
