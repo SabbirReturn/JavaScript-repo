f1c7e56b0df44b1da1a98cfc5128a309612c6afc
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-04-25 15:56:16 +0600

    get users in api and show the value in display

---
+++ explore-api/js/index.js
@@ -0,0 +1,16 @@
+function users(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => displayUser2(users))
+}
+function displayUser2(users){
+    let userContainer = document.getElementById('user-container')
+    for(let user of users){
+        console.log(user)
+        let ul = document.createElement('ul')
+        let li = document.createElement('li')
+        li.innerText = user.name;
+        userContainer.appendChild(ul);
+        ul.appendChild(li)
+    }
+}
