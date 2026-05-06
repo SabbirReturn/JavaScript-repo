f5bcc29d6ee3b9fb151b625af809e9d734d85649
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-07 03:43:55 +0600

    event

---
+++ more-javaScript/event_loop.js
@@ -0,0 +1,11 @@
+function a(){
+    console.log('aa')
+    b()
+    console.log('c')
+}
+function b(){
+    console.log('bb')
+    console.log('b')
+}
+
+a()
--- more-javaScript/interval.js
+++ more-javaScript/interval.js
@@ -1,14 +1,15 @@
-console.log(1)
-console.log(2)
-console.log(3)
-setTimeout(()=>{
-    console.log(4)
-},4000)
-console.log(5)
-console.log(6)
-let num = 0;
-let clockId = setInterval(()=>{
-    num++
-    // console.log(clockId,num)
-},2000
-)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// setTimeout(()=>{
+//     console.log(4)
+// },4000)
+// console.log(5)
+// console.log(6)
+// let num = 0;
+// let clockId = setInterval(()=>{
+//     num++
+//     console.log(clockId,num)
+//     clearInterval(clockId)
+// },2000
+// )
