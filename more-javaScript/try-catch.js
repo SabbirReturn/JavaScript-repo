20e6e8f2c87ac6204d1eb79ab031a73e2c3810d8
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-08 05:13:43 +0600

    again

--- more-javaScript/try-catch.js
+++ more-javaScript/try-catch.js
@@ -1,26 +1,51 @@
-function checkAge(){
-    let ageField = document.getElementById('age');
-    let ageText = ageField.value;
-    console.log(ageText)
-    let errorTag = document.getElementById('error')
+// function checkAge(){
+//     let ageField = document.getElementById('age');
+//     let ageText = ageField.value;
+//     console.log(ageText)
+//     let errorTag = document.getElementById('error')
     
+//     try{
+//         // console.log(bBaria)
+//         let age = parseInt(ageText);
+//         if(isNaN(age)){
+//             throw `Please enter a number`
+//         }
+//         else if(age<18){
+//             throw `Baccha kacca not Allow`
+//         }
+//         errorTag.innerHTML = '';
+//     }
+//     catch(err){
+//         console.log('Error:', err)
+//         errorTag.innerHTML = 'Error:' + err
+//     }
+//     finally{
+//         console.log('All done')
+//     }
+//     // console.log(9999) 
+// }
+
+let checkAge = ()=>{
+    let inputField = document.getElementById('age');
+    let ageText = inputField.value;
+    console.log(ageText);
+    let errorTag = document.getElementById('error')
+
     try{
-        // console.log(bBaria)
-        let age = parseInt(ageText);
+        let age = parseInt(ageText)
         if(isNaN(age)){
-            throw `Please enter a number`
+            throw `Please Enter a number`
         }
         else if(age<18){
-            throw `Baccha kacca not Allow`
+            throw `Tomar boyos hoi nai`
         }
-        errorTag.innerHTML = '';
+        errorTag.innerHTML = ''
     }
     catch(err){
         console.log('Error:', err)
-        errorTag.innerHTML = 'Error:' + err
+        errorTag.innerHTML = `Error:` + err
     }
     finally{
         console.log('All done')
     }
-    // console.log(9999) 
 }
