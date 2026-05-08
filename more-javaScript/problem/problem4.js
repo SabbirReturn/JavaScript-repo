5196cfed3a07a798f552807963f2fe7e93b6a49e
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-08 21:03:44 +0600

    problem4

--- more-javaScript/problem/problem4.js
+++ more-javaScript/problem/problem4.js
@@ -1,5 +1,15 @@
 let loadJoke = async()=>{
     let res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single')
-    let data = await res.json();
-    console.log(data)
+    let datas = await res.json();
+    let result = datas.joke
+    disPlayShow(result)
 }
+
+let disPlayShow = (datas)=>{
+    let container = document.getElementById('container')
+    let description = document.createElement('div');
+    description.innerText = datas;
+    container.appendChild(description)
+}
+
+loadJoke();
