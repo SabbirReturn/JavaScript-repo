ee7d5cb96522528bf87b472c26fd35775e8f5194
SabbirReturn <mdsabbirhasan39@gmail.com>
2026-05-18 03:23:29 +0600

    again

--- local-storage/script/practice_cart.js
+++ local-storage/script/practice_cart.js
@@ -7,6 +7,7 @@ let addProduct= ()=>{
     productField.value = '';
     quantityField.value = '';
     disPlayShow(product,quantity)
+    saveLocalStorage(product,quantity)
 }
 
 let disPlayShow = (product,quantity)=>{
@@ -14,4 +15,20 @@ let disPlayShow = (product,quantity)=>{
     let li = document.createElement('li')
     li.innerText = `${product} : ${quantity}`
     productContainer.appendChild(li);
+}
+    let getProductCart = ()=>{
+        let cart = {}
+        let getProduct = localStorage.getItem('cart');
+        if(getProduct){
+            cart = JSON.parse(getProduct)
+        }
+        return cart;
+    }
+
+let saveLocalStorage = (product,quantity)=>{
+    let cart = getProductCart();
+    cart[product] = quantity;
+    let cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart',cartStringified)
+    
 }
